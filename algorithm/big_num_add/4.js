3a3aadd89360053919a720cd7b0d41063101f175
miao-key <19500298716@163.com>
2025-05-31 16:59:46 +0800

    深度学习了大数相加面试题

---
+++ algorithm/big_num_add/4.js
@@ -0,0 +1,10 @@
+const bigNum = 123456789012345678901234567890123456789n
+// BigInt 以字符串方式传参
+// BigInt 申明方式 函数申明
+// BigInt 简单数据类型，不是对象，不是构造函数
+const theNum = BigInt("123456789012345678901234567890123456789")
+console.log(bigNum,theNum,typeof bigNum,typeof 1);
+console.log(bigNum + 1n);
+
+
+
