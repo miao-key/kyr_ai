8b308405f65a24785e8b0559a1f18093176a52ac
miao-key <19500298716@163.com>
2025-06-08 15:24:27 +0800

    学习了原型

---
+++ JS/map/1.js
@@ -0,0 +1,12 @@
+// NaN Not a Number
+// parseInt 字符串转数字 JS 前端场景
+// input输入-> Number
+console.log(['1','2','3'].map(parseInt))
+// [1, 2, 3]
+
+
+
+
+
+
+
---
+++ JS/map/2.js
@@ -0,0 +1,16 @@
+// parseInt num 
+['1','2','3'].map((num,index,arr) =>{
+    console.log(num,index,arr)
+    return num
+})
+
+console.log(parseInt('1',0,['1','2','3']))
+console.log(parseInt('2',1,['1','2','3']))
+console.log(parseInt('3',2,['1','2','3']))
+
+
+
+
+
+
+
