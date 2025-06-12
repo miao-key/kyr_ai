f361533cc6b957e7c83d066531526d8a357154d0
miao-key <19500298716@163.com>
2025-06-12 21:30:22 +0800

    学习Promise

--- Leetcode/1.js
+++ Leetcode/1.js
@@ -1,8 +1,22 @@
 /**
- * @param {string} s
- * @param {string} p
+ * @param {number[]} nums
+ * @param {number} k
  * @return {number[]}
  */
-var findAnagrams = function(s, p) {
-    
+var maxSlidingWindow = function(nums, k) {
+    const ans = [];
+    const q = [];
+    for(let i = 0;i < nums.length;i++){
+        while(q.lenght && nums[q[q.length-1]] <= nums[i]){
+            q.pop();
+        }
+        q.push(i);
+        if(i - q[0] >=k){
+            q.shift();
+        }
+        if(i >= k-1){
+            ans.push(nums[q[0]]);
+        }
+    }
+    return ans;
 };
