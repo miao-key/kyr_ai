6319254c69e22e270438fdff415ecee1c3142ef2
miao-key <19500298716@163.com>
2025-07-02 22:08:51 +0800

    存储，react1

---
+++ JS/storage/cookie-demo/app.js
@@ -0,0 +1,12 @@
+// es6 模块化
+// mjs 后缀使用es6模块化
+// 模块化是语言的能力
+// node 默认不支持es6 模块化
+// node 最新版本支持了 22
+// node 准备跟require commonjs say goodbye
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+  res.end('hello http server');
+});
+server.listen(1314);
