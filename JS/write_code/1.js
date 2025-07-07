cfd2da82c668cc545f025053926c3324ff2770b3
miao-key <19500298716@163.com>
2025-07-07 09:29:32 +0800

    智能前端之图片识别

---
+++ JS/write_code/1.js
@@ -0,0 +1,48 @@
+// 完成的功能
+// function objectFactory() {
+//     var obj = {};
+//     // 类数组上没有shift方法，所以借用数组的shift方法
+//     var Constructor = [].shift.call(arguments); // 构造函数
+//     obj.__proto__ = Constructor.prototype
+//     var ret = Constructor.apply(obj,arguments)
+//     // || null 的情况 仍然会返回object 构造函数 return 简单类型，忽略
+//     return typeof ret === 'object' ? ret || obj : obj
+// }
+
+// es6 版本
+function objectFactory(Constructor,...args) {
+    var obj = {};
+    // 类数组上没有shift方法，所以借用数组的shift方法
+    // var Constructor = [].shift.call(arguments); // 构造函数
+    obj.__proto__ = Constructor.prototype
+    var ret = Constructor.apply(obj,args)
+    // || null 的情况 仍然会返回object 构造函数 return 简单类型，忽略
+    return typeof ret === 'object' ? ret || obj : obj
+}
+
+function Person(name,age){
+    this.name = name;
+    this.age = age;
+    // return 1;
+    return {
+        name: name,
+        age: age,
+        label: '六百六十六'
+    }
+}
+
+Person.prototype.sayHi = function(){
+    console.log(`你好，我是${this.name}`)
+}
+
+let p1 = new Person('法外狂徒',28)
+console.log(p1)
+// p.sayHi()
+
+let p = objectFactory(Person,'张三',18)
+console.log(p)
+p.sayHi()
+console.log(p instanceof Person)
+// new Person(...) -> function [[construct]] -> {} &&this ->{} -> [[call]] -> {}.__proto__ = Constructor.prototype
+// -> return {}
+
