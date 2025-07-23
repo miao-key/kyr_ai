5225de32bfa082dbcc096a38c0a5fce36a7e93bb
miao-key <19500298716@163.com>
2025-07-23 17:31:22 +0800

    jwt

--- react/jwt-demo/src/App.jsx
+++ react/jwt-demo/src/App.jsx
@@ -1,13 +1,43 @@
-import { useState } from 'react'
-
+import { 
+  useState,
+  useEffect,
+  lazy,
+  Suspense
+ } from 'react'
 import './App.css'
+import NavBar from './components/NavBar'
+
+import {
+  Routes,
+  Route
+} from 'react-router-dom'
+
+const Home = lazy(() => import('./views/Home'))
+const Login = lazy(() => import('./views/Login'))
+const Pay = lazy(() => import('./views/Pay'))
+const RequireAuth = lazy(() => import('./components/RequireAuth'))
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    // (async () => {
+    //   const res = await getUser();
+    //   console.log(res);
+    // })()
+  },[])
 
   return (
     <>
-      
+    <NavBar />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/pay" element={
+          <RequireAuth>
+            <Pay />
+          </RequireAuth>} />
+      </Routes>
+    </Suspense>
     </>
   )
 }
