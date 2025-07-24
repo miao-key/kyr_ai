196cd3289e340dfed040700eb46f6f6cd40109cd
miao-key <19500298716@163.com>
2025-07-24 21:29:43 +0800

    路由配置和tabbar组件

---
+++ react/trip/src/components/MainLayout/index.jsx
@@ -0,0 +1,63 @@
+import {
+  useState,
+  useEffect
+} from 'react'
+import {
+  Tabbar
+} from 'react-vant'
+import {
+    Outlet,
+    useNavigate,
+    useLocation
+} from 'react-router-dom'
+import {
+  HomeO,
+  Search,
+  FriendsO,
+  SettingO,
+  UserO
+} from '@react-vant/icons'
+
+// 菜单栏配置
+const tabs = [
+    {icon: <HomeO/>,title: '首页',path: '/home'},
+    {icon: <Search/>,title: '特惠专区',path: '/discount'},
+    {icon: <FriendsO/>,title: '我的收藏',path: '/collection'},
+    {icon: <SettingO/>,title: '旅程',path: '/trip'},
+    {icon: <UserO/>,title: '我的账户',path: '/account'}
+]
+const MainLayout = () => {
+  const [action, setAction] = useState(0)
+  const navigate = useNavigate();
+  useEffect(() => {
+    console.log(location.pathname,'///////')
+    // es6的使用power
+    const index = tabs.findIndex(
+      tab => location.pathname.startsWith(tab.path)
+    );
+    setAction(index)
+  }, []);
+  return (
+    <>
+        <Outlet />
+       {/* tabbar */}
+       <Tabbar value={action} onChange={
+        (key) => { 
+          setAction(key);
+          navigate(tabs[key].path);
+        }
+       }>
+        {tabs.map((tab, index) => (
+          <Tabbar.Item
+           key={index}
+           icon={tab.icon}
+          >
+            {tab.title}
+          </Tabbar.Item>
+        ))}
+       </Tabbar>
+    </>
+  )
+}
+
+export default MainLayout
--- react/trip/src/main.jsx
+++ react/trip/src/main.jsx
@@ -3,9 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'lib-flexible'  // 移动端适配
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </StrictMode>,
 )
