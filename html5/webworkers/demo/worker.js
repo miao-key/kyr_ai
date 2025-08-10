4a91cb0c80c5c5585bda42c3a93dd3d06df546de
miao-key <19500298716@163.com>
2025-08-10 22:28:29 +0800

    webworkers（多线程API与图片压缩场景）

---
+++ html5/webworkers/demo/worker.js
@@ -0,0 +1,12 @@
+// 不是JS 单线程
+// Worker 线程 复杂或耗性能的计算
+// 这个能力来自于浏览器
+// js 还是单线程，只不过在复杂计算时候用worker 线程
+// 不可以使用document,也没有this
+// 线程间的通信，信息机制
+// console.log(this,'////');
+// console.log(document.getElementById('box'));
+self.onmessage = function(event) {
+    console.log(event.data,'////');
+    self.postMessage('hello from worker');
+}
