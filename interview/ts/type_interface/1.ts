8556dbbfd25cd0d3067d4c2055d772fc8012b5cc
miao-key <19500298716@163.com>
2025-08-27 23:00:14 +0800

    ts泛型的学习+ type + interface

---
+++ interview/ts/type_interface/1.ts
@@ -0,0 +1,17 @@
+// js 弱类型，容易出问题 
+// ts带来类型的约束
+// ts 是微软 想让java 工程师写前端 
+// react + ts 是开发的标配
+// 自定义类型
+// interface 关键字
+interface User {
+    name: string;
+    age: number;
+}
+// 相同点，都可以申明自定义类型
+type UserType = {
+    name: string;
+    age: number;
+}
+const u1:User = {name: 'Alice',age: 20};
+const u2:UserType = {name: 'Bob',age: 21};
---
+++ interview/ts/type_interface/5.ts
@@ -0,0 +1,9 @@
+interface AddFn {
+    (a: number, b: number): number
+}
+
+type AddType = (a: number, b: number) => number
+
+type NumberOther = number 
+
+let NumberOther = 22
