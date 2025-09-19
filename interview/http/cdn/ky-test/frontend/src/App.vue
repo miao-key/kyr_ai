ec5e4551db0e4f544655768f8e2cf63a8bc8186e
miao-key <19500298716@163.com>
2025-09-19 17:07:23 +0800

    跨域

---
+++ interview/http/cdn/ky-test/frontend/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+// CORS policy 同源策略
+// Cross-Origin Resource Sharing policy
+async function fetchUser() {
+  // 前端知道访问的地址和域名
+  // 委托vite 工程化 帮解决下
+    const res = await fetch('http://localhost:3000/api/user');
+    const data = await res.json();
+    console.log(data);
+}
+
+fetchUser();
+</script>
+
+<template>
+    <div>
+
+    </div>
+</template>
