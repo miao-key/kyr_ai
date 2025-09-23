bc0cc35a72e6c179ab4bb3d91155748d36b98953
miao-key <19500298716@163.com>
2025-09-23 19:38:53 +0800

    删除回文子序列算法题学习

---
+++ interview/algorithm/del_palindrome/1.js
@@ -0,0 +1,25 @@
+function validPalindrom(s) {
+    let left = 0,
+      right = s.length -1;
+    
+    const isPal = (str, i, j) => {
+      while(i < j) {
+        if (str[i] !== str[j]) return false;
+        i++;
+        j--;
+      }
+      return true;
+    }
+    while(left < right) {
+      if (s[left] === s[right]) {
+        left++;
+        right--;
+      } else {
+        return isPal(s, left + 1, right) || isPal(s, left, right - 1);
+      }
+    }
+  
+    return true;
+  }
+  
+  console.log(validPalindrom('abca'));
