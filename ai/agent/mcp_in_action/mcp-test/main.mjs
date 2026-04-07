921422fe83a7f53ae8dccd9ce54e792b0080ccb9
miao-key <19500298716@163.com>
2026-04-07 08:18:51 +0800

    高德地图mcp配置，文件写入，浏览器展开，大模型一体化调用完成

---
+++ ai/agent/mcp_in_action/mcp-test/main.mjs
@@ -0,0 +1,92 @@
+import 'dotenv/config';
+import {MultiServerMCPClient} from '@langchain/mcp-adapters';
+import { ChatOpenAI } from '@langchain/openai';
+import chalk from 'chalk';
+
+import {
+    HumanMessage,
+    SystemMessage,
+    ToolMessage,
+} from '@langchain/core/messages';
+
+const model = new ChatOpenAI({
+    modelName: process.env.MODEL_NAME,
+    apiKey: process.env.OPENAI_API_KEY,
+    configuration: {
+        baseURL: process.env.OPENAI_BASE_URL,
+    }
+});
+
+const mcpClient = new MultiServerMCPClient({
+    mcpServers: {
+        "amap-maps-streamableHTTP": {
+            "url": `https://mcp.amap.com/mcp?key=${process.env.AMAP_MAPS_API_KEY}`
+        },
+        // mcp 官方提供 
+        "filesystem": {
+            "command": "npx",
+            "args": [
+                "-y",
+                "@modelcontextprotocol/server-filesystem",
+                "/Users/why/Desktop/lesson_ai/ai/agent/mcp_in_action/mcp-test"
+            ]
+        },
+        "chrome-devtools": {
+            "command": "npx",
+            "args": [
+                "-y",
+                "chrome-devtools-mcp@latest",
+            ]
+        }
+    },
+});
+
+const tools = await mcpClient.getTools();
+const modelWithTools = model.bindTools(tools);
+
+async function runAgentWithTools(query,maxIterations = 30) {
+    const messages = [
+        new HumanMessage(query),
+    ];
+    for (let i = 0; i < maxIterations; i++) {
+        console.log(chalk.bgGreen('正在等待AI思考...'));
+        const response = await modelWithTools.invoke(messages);
+        messages.push(response);
+
+        if (!response.tool_calls || response.tool_calls.length === 0) {
+            console.log(`\n AI 最终回复：\n ${response.content}\n`);
+            return response.content;
+        }
+
+        console.log(chalk.bgBlue(`检测到 ${response.tool_calls.length} 个工具调用`));
+        console.log(chalk.bgBlue(`工具调用：${response.tool_calls.map(t => t.name).join(', ')}`));
+        
+        for (const toolCall of response.tool_calls) {
+            const foundTool = tools.find(t => t.name === toolCall.name);
+            if (foundTool) {
+                const toolResult = await foundTool.invoke(toolCall.args);
+                let contentStr;
+                if (typeof toolResult === 'string') {
+                    contentStr = toolResult;
+                } else {
+                    contentStr = JSON.stringify(toolResult);
+                }
+                messages.push(new ToolMessage({
+                    content: contentStr,
+                    tool_call_id: toolCall.id,
+                }));
+            }
+        }
+    }
+    return messages[messages.length - 1].content;
+}
+
+// await runAgentWithTools('北京南站附近的酒店，以及去的路线');
+// await runAgentWithTools(`北京南站附近的2个酒店，以及去的路线，
+//     路线规划生成保护文档保存到/Users/why/Desktop/lesson_ai/ai/agent/mcp_in_action/mcp-test/readme.md文件中
+//     `);
+await runAgentWithTools(`
+北京南站附近的2个酒店，附近的3个酒店，拿到酒店图片，展开浏览器，展示每个酒店的图片，
+每个tab一个url展示，并且把那个页面标题改为酒店名
+`);
+await mcpClient.close();
